eea0b721dc85a956841fd6be81ff511e9d2655eb
Spiderman <talent111dev@gmail.com>
2022-12-09 00:58:59 +0000

    Update tracking time record

--- app/Http/Controllers/Api/CaseRecordController.php
+++ app/Http/Controllers/Api/CaseRecordController.php
@@ -261,6 +261,7 @@ class CaseRecordController extends Controller
         $case_record = CasesRecord::find($RecordID);
         if($case_record){
             $case_record->end_time = Carbon::now()->format('H:i:s');
+            $case_record->interval_time = $request->interval_time;
             $case_record->save();
         }else{
             $response = array();
