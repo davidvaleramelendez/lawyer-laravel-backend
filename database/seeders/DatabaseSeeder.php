fc9d5a6e8b5fd9c0172583765b02a06ee1258343
Kuldeep <kuldeep.strivedge@gmail.com>
2023-01-20 19:21:34 +0530

    Seeding file updated for cloud convert api key.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -113,5 +113,11 @@ class DatabaseSeeder extends Seeder
 
         \App\Models\Permissions::insert($permissions);
         /* /Permission seeding */
+
+        /* Pdf Api */
+        $pdfApis = ['key' => "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYWM3ODU3YjlkODZhNmMzOWQ4NDIyYWZiZWI4YjNlZWYyMWI4MjgyMjJmOTAyZTU4NTI4YmFmNTgxYjk4OTM3M2I4Y2JhNWNjN2I4ZGJhOTkiLCJpYXQiOjE2NTkzNTA0OTguNjkyMTA3LCJuYmYiOjE2NTkzNTA0OTguNjkyMTA5LCJleHAiOjQ4MTUwMjQwOTguNjg3MjY4LCJzdWIiOiI1NDUzNDg2NCIsInNjb3BlcyI6WyJ1c2VyLnJlYWQiLCJ1c2VyLndyaXRlIiwidGFzay5yZWFkIiwidGFzay53cml0ZSIsIndlYmhvb2sucmVhZCIsIndlYmhvb2sud3JpdGUiLCJwcmVzZXQucmVhZCIsInByZXNldC53cml0ZSJdfQ.n3kzFYSiDzSjkvzNduyWe-ZoaSucgTylpclP2O0oI4NKrA9Vd0qU6FAnOYq8MOK25MqbxnR-mkvQAGzNjVHCFv4xnWQq9lGzbs6N_ReEnjZbKi2Dntflr5KhYOu4NRo6EsW3Ko2p6dEBPqOowvh5BC7EGCmFlpHfXBtJ9vtLKXR-unfRdQLM-Zh6noYMf63uN8LvD71WRKUDJBchA4-7rrNIM5Fl0roR7SAZ1tHtBSu5tdnRMOgVomLoq3BGdWC6pWsI90IjeqjrORWQHM-kJez0__uytHo-2ohlTUWdzWB68Fq7To-VGXoiNPQSiPnaE8aPI1f_o8Tu28ngyedDcNKRPLYwkf1XmI5J_aGRsoMqBcqwEofSpPWOkLoJKqCwdpRGHdqckg4ImQ-9ACvQILS_QJYLB4i4_UmDy7sgGQ5Xo6MQl37WZKQOWw1bPkn9mNnoZNVAb_qmnCUHV6btdIxAlMN7JPZ-i1JbSoR6bBkb78RwxsNLqwrvE9EfNb-qyt6yfWyE5l5l6hNlqam06DYCjqrY-2Ev2EiVOr_v4ML3EtaXcPgOw0YFG_gf26D4MxKrlWaiu7KUmop2VtSo3HXSmtk6gXWxZkgcmNhP_Gf3P4Den3opsXbOAK7mWkwswxFsXKk0hh1TTdwrwKtxzlfCt496rQ6HCZKu-tbr4g4"];
+
+        \App\Models\PdfApi::insert($pdfApis);
+        /* /Pdf Api */
     }
 }
